bec7f33d637684c64b01c12e55d932a38381a1fe
mattgilgo <70973398+mattgilgo@users.noreply.github.com>
2021-11-07 22:53:02 -0500

    Final implementation

--- src/World.java
+++ src/World.java
@@ -3,9 +3,19 @@ import java.util.*;
 public class World {
 
     String[][] backgroundMap;
+    String[][] currentMap;
 
     public World() {
         this.backgroundMap = new String[][] 
+                    {{"M","I","C","M","C","M","M","C"},
+                    {"C","C","M","C","C","M","M","M"},
+                    {"I","M","C","C","C","C","M","C"},
+                    {"C","M","M","M","M","C","M","C"},
+                    {"C","C","M","M","M","I","M","I"},
+                    {"M","I","C","I","I","C","C","C"},
+                    {"I","C","C","C","I","I","C","C"},
+                    {"C","M","C","C","M","M","I","I"}};
+        this.currentMap = new String[][] 
                     {{"M","I","C","M","C","M","M","C"},
                     {"C","C","M","C","C","M","M","M"},
                     {"I","M","C","C","C","C","M","C"},
@@ -20,9 +30,14 @@ public class World {
         return this.backgroundMap[r][c];
     }
 
+    public void setHeroSpace(int r, int c) {
+        this.currentMap[r][c] = "H";
+    }
+
     public boolean isThereAMonster() {
         Random r = new Random();
-        boolean check = r.nextInt(100) < 50;
+        int ran = r.nextInt(100);
+        boolean check = ran < 50;
         if (check) {
             return true;
         } else {
@@ -30,10 +45,14 @@ public class World {
         }
     }
 
+    public String[][] resetMap() {
+        return this.backgroundMap;
+    }
+
+
      /* 
      * A method used to print the map of the World.
      */   
-
     public void printMap(int n) {
         System.out.print(" ");
         for (int i = 0; i < n; i++) {
@@ -44,7 +63,7 @@ public class World {
         for (int r = 0; r < n; r++) {
             System.out.print(r+1 + " | ");
             for (int c = 0; c < n; c++) {
-                System.out.print(this.backgroundMap[r][c] + " | ");
+                System.out.print(this.currentMap[r][c] + " | ");
             }
             System.out.println();
         }
